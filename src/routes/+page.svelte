19767602391d0e41f89b2f70b92ddfa61607a0ee
sillvva <matt.dekok@gmail.com>
2025-11-16 15:46:55 -0600

    Simplify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
-<script lang="ts">
-	import { getFoo } from './test.remote.js';
-</script>
 <svelte:boundary>
-	{@const foo = await getFoo()}
+	{@const foo = await 1}
 	<p>Everything here is doubled</p>
 	<button class="border" onclick={() => console.log("I'm clickable")}>
 		Click me and check log
